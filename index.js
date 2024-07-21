066440800d7ac97a740e9a1d126839d6637a5b21
StephNdungu <stephy.wanjiku@gmail.com>
2024-07-22 00:41:14 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+    
+  console.log(messages);
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
